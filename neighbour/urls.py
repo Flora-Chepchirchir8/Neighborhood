c2e68e9c86307f16de97ef9fd68fe8f15b7ff8df
Flora-Chepchirchir8 <flora.chepchirchir@moringaschool.com>
2022-06-20 10:51:25 +0300

    configure urls path

--- neighbour/urls.py
+++ neighbour/urls.py
@@ -14,8 +14,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views 
 from django.urls import path
+from hood import views
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.welcome, name='welcome'),
+    path('index',views.index, name='index'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('profile-update',views.update_profile, name='update_profile'),
+    path('profile/<pk>',views.profile, name = 'profile'),
+    path('create-hood',views.createhood, name='createhood'), 
+    path('hood/<id>',views.neighbourhood, name = 'neighbourhood'),
+    path('join_neighbourhood/<id>', views.join_neighbourhood, name='join-neighbourhood'),
+    path('business/<id>',views.createbusiness, name = 'create-business'),
+    path('post/<hood_id>',views.post, name = 'post'),
+    path('change_neighbourhood/<id>', views.change_neighbourhood, name='change-neighbourhood'),
+    path('search/',views.search_results, name='search_results'),
+    path('logout/', views.logout_user, name='logout'),
+
+
 ]

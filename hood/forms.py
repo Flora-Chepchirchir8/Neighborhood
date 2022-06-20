b2cc5fbe6e0dbfd88ad2b1ce414a2ff3ae5ecb46
Flora-Chepchirchir8 <flora.chepchirchir@moringaschool.com>
2022-06-20 11:20:58 +0300

    generate forms

---
+++ hood/forms.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from django import forms
+from .models import Profile,NeighbourHood,Business,Post
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields=['profile_picture','bio','email','phone_no'] 
+class HoodForm(forms.ModelForm):
+    class Meta:
+        model = NeighbourHood
+        fields=['name','location','description','image','health_center','health_email','health_contact']         
+        
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        fields=['image','name','email','phone_no']            
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields=['title','info']       
+       
